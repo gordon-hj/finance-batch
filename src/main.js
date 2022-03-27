dc6b951ab49ea2e09dbb842d4a6b145431f12c43
gordon.hj <gordon_hj@naver.com>
2022-03-28 00:11:06 +0900

    set source and main app

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// TODO phase
+const phase = 'dev';
+console.log("Request phase =>", phase);
+const cron = require('node-cron');
+const rdb = require('./datasource/rdb')(phase);
+const config_repository = require('./repository/config_repository')(rdb);
+
+// 1분에 한 번씩 배치
+cron.schedule('* * * * *', function () {
+    config_repository.selectByKey('batch-allow')
+    .then((result) => {
+        console.log(result);
+        if(result.value == true) {
+            console.log('배치 실행');
+        } else {
+            console.log('배치 방어 처리 되어있습니다. 실행 시키려면 config를 수정하세요.');
+        }
+    })
+}).start();
+  
