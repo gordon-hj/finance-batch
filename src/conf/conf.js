9fa05e9830e71c7ae120295bd418b0dfc435a90e
gordon.hj <gordon_hj@naver.com>
2022-03-28 00:09:56 +0900

    set conf

---
+++ src/conf/conf.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+module.exports = function(phase) { 
+    var phaseFilename = null;
+    // TODO 분기 예쁘게 만들기
+    switch(phase) {
+        case 'dev' || 'production':
+            phaseFilename = phase;
+            break;
+        default :
+            console.log('Unexpected requested phase => ' + phase);
+            phaseFilename = 'dev';
+    }
+    console.log('Service phase => ' + phaseFilename);
+
+    const jsonFile = fs.readFileSync('src/conf/' + phaseFilename + '.json', 'utf8');
+    return JSON.parse(jsonFile);
+}
