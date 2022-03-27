57da9b4ab50d010975ef70ea72f7f26878a850ec
gordon.hj <gordon_hj@naver.com>
2022-03-28 00:10:19 +0900

    set datasource

---
+++ src/datasource/rdb.js
@@ -0,0 +1,45 @@
+// datasource configuration
+const mysql = require('mysql');
+
+var pool = null;
+
+module.exports = (phase) => {
+    // TODO CONF 로딩 방식 예쁘게 만들기
+    var conf = require('../conf/conf')(phase).datasource;
+    pool = mysql.createPool({
+        connectionLimit: conf.connectionLimit,
+        host: conf.host,
+        user: conf.user,
+        password: conf.password,
+        database: conf.database,
+        debug: conf.debug,
+    });
+    console.log("Mysql Datasource connected => host : " + conf.host + ", user : " + conf.user + ", database : " + conf.database);
+    return this;
+}
+
+exports.query = (query, data, success, fail) => {
+    pool.getConnection((err, conn) => {
+        if(err) {
+            if(conn) conn.release();
+            queryError(query, data, err);
+            fail();
+            return;
+        }
+        conn.query(query, data, (err, result) => {
+            conn.release();
+            if(err) {
+                queryError(query, data, err);
+                fail();
+                return;
+            }
+            success(result);
+            return;
+        });
+    })
+}
+
+queryError = (query, data, err) => {
+    console.log("Query failed => query: " + query + ', data: ' + JSON.stringify(data));
+    console.log(err);
+}
---
+++ src/repository/config_repository.js
@@ -0,0 +1,32 @@
+var ds = null;
+module.exports = (datasource) => { 
+    ds = datasource;
+    return this;
+};
+
+const table = 'config';
+const columns = ['id', 'key', 'value'];
+class Config {
+    constructor(id, key, value) {
+        this.id = id;
+        this.key = key;
+        this.value = (value == 'true')? true : false;
+    }
+}
+
+exports.selectByKey = (key) => {
+    return new Promise((resolve, reject) => 
+        ds.query(
+            query = 'SELECT ?? FROM ?? WHERE `key` = ?',
+            data = [columns, table, key],
+            (result) => resolve(parseRows(result)[0]),
+            () => reject(),
+        )
+    )
+}
+
+parseRows = (result) => {
+    return result.map((v) => {
+        return new Config(v.id, v.key, v.value);
+    });
+}
