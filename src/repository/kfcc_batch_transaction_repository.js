8646804ea2023191565ec5577a3559f112fe0ba9
gordon-hj <gordon_hj@naver.com>
2022-09-26 23:28:38 +0900

    배치 트랜잭션 수정
    네이밍 수정

--- src/repository/kfcc_batch_transaction_repository.js
+++ src/repository/kfcc_batch_transaction_repository.js
@@ -42,7 +42,7 @@ exports.save = (batchDate, type, typeId, status) => {
 }
 
 exports.saveAll = (batchDate, newTransactions, status) => {
-    if(values.length <= 0) return new Promise(resolve => { resolve() })
+    if(newTransactions.length <= 0) return new Promise(resolve => { resolve() })
     let insertValues = newTransactions.map(value => [batchDate, value.type, value.typeId, status]);
     return new Promise((resolve, reject) => 
         this.ds.query(
